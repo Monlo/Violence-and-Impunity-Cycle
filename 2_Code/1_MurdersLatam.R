ddd557bc947d593d1350f9d92a272fdcbc4e76d3
Monserrat López <lopezmonserrat.14@gmail.com>
2023-01-28 17:17:00 -0600

    add files

---
+++ 2_Code/1_MurdersLatam.R
@@ -0,0 +1,70 @@
+rm(list = ls())
+
+#------------------------------------------------------------------------------#
+# Impunidad Cero
+# Project:          Article Mexico's Violence and Impunity Cycle 
+# Date:             January 2023
+# Authors:          Monserrat López and Helga Jáuregui
+# Section:          Intentional Murders Worldwide
+# Source:           UNODC: https://dataunodc.un.org/dp-intentional-homicide-victims 
+#------------------------------------------------------------------------------#
+
+## Setup ----
+Sys.setlocale("LC_ALL", "es_ES.UTF-8") # locale para Mac
+Sys.setlocale("LC_ALL", "Spanish") # locale para Windows
+options(scipen = 999)
+
+## Packages ----
+pacman::p_load(tidyverse, janitor, readxl, ggrepel,svglite)
+
+## Data ----
+
+### Import:
+
+# Victims of intentional homicide Data 
+db_1 <- read_excel("1_Data/data_cts_intentional_homicide.xlsx", 
+                   skip = 2) %>% 
+  clean_names() 
+
+### |- ¿Cuál es el panorama mundial? ----
+
+### Evolution of intentional murder rates in Latin America
+db <- db_1 %>% 
+  filter(indicator == "Victims of intentional homicide" & 
+           subregion == "Latin America and the Caribbean" & 
+           sex=="Total" &
+           dimension=="Total" &
+           unit_of_measurement == "Rate per 100,000 population"&
+           country %in% c("Colombia", "Honduras", "Mexico", "Brazil", 
+                          "Guatemala","El Salvador")) %>% 
+  mutate(color = if_else(condition = country == "Mexico", true = "1", false = "0"),
+         etiqueta = if_else(condition = year == 2020, 
+                            true = fct_recode(.f = as_factor(country), "Mexico" = "Mexico", 
+                                              "Brazil" = "Brazil"), 
+                            false = as_factor(NA))) 
+
+db %>% 
+  ggplot(aes(x = year, y = round(value, 1))) +
+  geom_point(aes(colour = color), size = 3) + 
+  geom_line(aes(color = color, group = country), size = 1) +
+  scale_color_manual(values = c("grey70", "#A087C3")) +
+  geom_text_repel(aes(label = etiqueta), nudge_x = .7, size = 8) +
+  annotate(geom = "text", label = "El Salvador", x = 2019, y = 35, size = 8)+
+  scale_y_continuous(limits = c(0,110), 
+                     breaks = seq(from = 0, to = 110, by = 10)) +
+  scale_x_continuous(limits = c(2008,2022),
+                     breaks = 2008:2020) +
+  labs(title = "Intentional homicide rates in selected countries in Latin America and the Caribbean",
+       subtitle = "in number of homicides per 100,000 inhabitants",
+       caption = "Source: UNODC") +
+  theme_minimal() +
+  theme(legend.position = "none", 
+        axis.title = element_blank(),
+        plot.title = element_text(size = 40, face = "bold"),
+        plot.subtitle = element_text(size = 30, face = "bold"),
+        plot.title.position = "plot",
+        axis.text = element_text(size = 25),
+        plot.caption = element_text(size = 20, hjust = 0))
+
+ggsave("3_Graphs/01_tasa_hom_latam.png", width = 18, height = 12, dpi = 200) 
+
---
+++ 2_Code/2_TypeofWeapon.R
@@ -0,0 +1,88 @@
+rm(list = ls())
+
+#------------------------------------------------------------------------------#
+# Impunidad Cero
+# Project:          Article Mexico's Violence and Impunity Cycle 
+# Date:             January 2023
+# Authors:          Monserrat López and Helga Jáuregui
+# Section:          Intentional Murders Committed with Firearms in Mexico
+# Source:           SESNSP: https://www.gob.mx/sesnsp/acciones-y-programas/victimas-nueva-metodologia?state=published 
+#------------------------------------------------------------------------------#
+
+# 0. Setup  ----
+Sys.setlocale("LC_ALL", "es_ES.UTF-8") # Para Mac
+Sys.setlocale("LC_ALL", "Spanish") # Para Windows
+options(scipen = 999)
+
+# Packages:
+pacman::p_load(readxl, tidyverse, janitor, treemapify,stringr)
+
+
+# 1. Load data  ----
+
+# Cargar base de datos de víctimas en las carpetas de investigación
+victims <- read_excel("1_Data/Estatal-V¡ctimas-2015-2022_dic2022.xlsx")%>% 
+  clean_names()
+
+victims %>% glimpse()
+
+# 2. Cleaning -----------------------------------------------------
+
+# Sum by month to calculate the year total 
+victims$total <- rowSums(victims[10:21])
+
+# Filter by type of crime (Intentional murders and Feminicides)
+db <- victims %>% 
+  filter(subtipo_de_delito == "Homicidio doloso" | subtipo_de_delito== "Feminicidio") 
+
+# Group by  por modality, state, sex and year
+
+db <- db %>% 
+  filter(ano != 2022 & sexo != "No identificado") %>% 
+  group_by(ano, subtipo_de_delito, clave_ent, entidad, sexo, modalidad) %>% 
+  summarise(total = sum(total, na.rm = T)) %>% 
+  ungroup() %>% 
+  arrange(clave_ent, ano)
+
+
+# Intentional murders in  2015 adn 2021 by sex and modality
+
+db_1 <- db %>% 
+  filter(subtipo_de_delito == "Homicidio doloso" & ano %in% c(2015, 2021)) %>% 
+  group_by(ano,sexo, modalidad) %>% 
+  summarise(tot_nacional = sum(total)) %>% 
+  ungroup() %>% 
+  group_by(ano,sexo) %>% 
+  mutate(percentage = round(tot_nacional/sum(tot_nacional)*100, 0),
+         modalidad = factor(x = modalidad, levels = c("No especificado", "Con arma blanca", "Con otro elemento","Con arma de fuego")))  %>%
+  mutate(modalidad = str_replace(modalidad, "No especificado", "Not specified")) %>%
+  mutate(modalidad = str_replace(modalidad, "Con arma blanca", "Handguns")) %>%  
+  mutate(modalidad = str_replace(modalidad, "Con arma de fuego", "Firearms")) %>%
+  mutate(modalidad = str_replace(modalidad, "Con otro elemento", "Other weapon")) %>%
+  mutate(sexo = str_replace(sexo, "Hombre", "Men")) %>%
+  mutate(sexo = str_replace(sexo, "Mujer", "Women")) %>%
+  ungroup() 
+  
+  # 2. Graph ----  
+
+db_1 %>%  
+  ggplot(aes(y = percentage, x = sexo)) +
+  geom_bar(aes(fill = modalidad),stat = "identity") +
+  scale_fill_manual(values = c("#9F86C2", "#CA98C7", "#FAB893", "#F9CB76")) +
+  facet_wrap(~ ano) +
+  labs(title = str_wrap(string = "Percentage of Victims of Intentional Homicide by Type of Weapon and Sex", width = 50),
+       fill = "Type of weapon",
+       caption = "Source: SESNSP") +
+  theme_minimal() +
+  theme(legend.position = "bottom",
+        legend.text = element_text(size = 20),
+        legend.title = element_text(size =20),
+        axis.title = element_blank(),
+        axis.text = element_text(size = 25),
+        plot.title = element_text(face = "bold", size = 40),
+        plot.title.position = "plot",
+        plot.subtitle = element_text(size = 35),
+        plot.caption = element_text(size = 20, hjust = 0),
+        strip.text = element_text(size = 28, face = "bold"))
+
+ggsave("3_Graphs/02_weapon.png", width = 16, height = 9, dpi = 200)
